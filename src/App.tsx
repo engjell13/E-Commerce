dbdaefa5a9d5df79d3fa2d3088deabde72e7e6ad
engjell13 <engjellm2001@gmail.com>
2023-07-09 21:02:31 +0200

    project setup, created folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Store from "./pages/Store";
+import Navbar from "./component/Navbar";
 
 function App() {
-  return <h1>Vite React Tailwindcss</h1>;
+  return (
+    <>
+      <Navbar />
+      <div className="container mx-auto">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/store" element={<Store />} />
+        </Routes>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/Store.tsx
@@ -0,0 +1,5 @@
+const Store = (): JSX.Element => {
+  return <h1>Store</h1>;
+};
+
+export default Store;
